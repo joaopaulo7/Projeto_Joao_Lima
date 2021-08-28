c4c46176cbe29eecfe01005cb83f9aace70ee357
joaopaulo7 <joaopauloff7@hotmail.com>
2021-08-28 14:19:01 -0300

    Criação da Classe C

---
+++ C.hpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+
+class A{
+	private:
+		String C1;
+		int C2;
+    
+    public:
+         String getC1(){
+            return this->C1;
+        }
+        int getC2(){
+            return this->C2;
+        }
+        
+        
+        void setC1(String c){
+            this->C1 = a;
+        }
+        void setC2(int c){
+           this->C2 = c;
+        }
+        
+        
+        void MC1(){
+            printf("MC1\n");
+        }
+        
+        void MC2(){
+            printf("MC2\n");
+        }
+};
